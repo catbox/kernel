d5225fecc31de18fcce9a5eddb7641e79402a6c6
cocolekat <cocolekat@gmail.com>
2015-03-04 17:43:36 -0500

    1) Made a utility class. 2) Added delete methods.
    Signed-off-by: cocolekat <cocolekat@gmail.com>

--- kernel/src/com/file/manip/FileManip.java
+++ kernel/src/com/file/manip/FileManip.java
@@ -11,90 +11,142 @@ import com.constants.FileManipConstants;
 
 public class FileManip {
 
-	private final File ROOT_PATH;
-	private String rootPathStr;
-	private Properties properties;
-	private File rootDirectory;
-	private File userDirectory;
-	private File userWishListDirectory;
+	private final static File ROOT_PATH = new File(FileManipConstants.USER_DATA_FILE_LOCATION);
+	private static String rootPathStr;
+	private static Properties properties;
+	private static File rootDirectory;
+	private static File userDirectory;
+	private static File userWishListDirectory;
 	
-	// Constructor - Creates the root directory for all users
-	public FileManip() {
+	// Creates the root directory for all users
+	public static void createRootDirectory() {
 		
-		ROOT_PATH = new File(FileManipConstants.USER_DATA_FILE_LOCATION);
-
 		try {
 			InputStream rootPathStream = new FileInputStream(ROOT_PATH);
 			properties = new Properties();
 			properties.load(rootPathStream);
+			rootPathStr = properties.getProperty(FileManipConstants.ROOT);
+			
+			rootDirectory = new File(rootPathStr);
+			
+			if(!rootDirectory.exists()) {
+				rootDirectory.mkdir();
+				System.out.println("Directory was created at: " + rootDirectory.getAbsolutePath());
+			}
+			else {
+				System.out.println("Directory already exists at: " + rootDirectory.getAbsolutePath() + " *** Directory not created!");
+			}
 		} 
 		catch(FileNotFoundException fileNotFoundException) {
 			System.out.println("FileNotFoundException: " + fileNotFoundException.getMessage());
 		} 
 		catch(IOException ioException) {
-			System.out.println("IOException: " + ioException);
+			System.out.println("IOException: " + ioException.getMessage());
 		}
-
-		rootPathStr = properties.getProperty(FileManipConstants.ROOT);
-		
-		rootDirectory = new File(rootPathStr);
-		if(!rootDirectory.exists()) {
-			rootDirectory.mkdir();
-			System.out.println("Directory was created at: " + rootDirectory.getAbsolutePath());
-		}
-		else {
-			System.out.println("Directory already exists at: " + rootDirectory.getAbsolutePath());
-		}
+		catch(Exception exception) {
+			System.out.println("IOException: " + exception.getMessage());
+		}		
 	}
 	
 	// Create the user's directory
-	public void createUserDirectory(String userDirectory) {
-		this.userDirectory = new File(rootPathStr + File.separator + userDirectory);
-		if(!this.userDirectory.exists()) {
-			this.userDirectory.mkdir();
-			System.out.println("User directory was created at: " + this.userDirectory.getAbsolutePath());
+	public static void createUserDirectory(String userRootDirectory) {
+		
+		userDirectory = new File(rootPathStr + File.separator + userRootDirectory);
+		
+		if(!userDirectory.exists()) {
+			userDirectory.mkdir();
+			System.out.println("User directory was created at: " + userDirectory.getAbsolutePath());
 		}
 		else {
-			System.out.println("User directory already exists at: " + this.userDirectory.getAbsolutePath());
+			System.out.println("User directory already exists at: " + userDirectory.getAbsolutePath() + " *** Directory not created!");
 		}
 	}
 	
 	// Create the user's wish list directory
-	public void createUserWishListDirectory(String userDirectory, String wishlist) {
-		this.userWishListDirectory = new File(rootPathStr + File.separator + userDirectory + File.separator + wishlist);
-		if(!this.userWishListDirectory.exists()) {
-			this.userWishListDirectory.mkdir();
-			System.out.println("Wish List was created at: " + this.userWishListDirectory.getAbsolutePath());
+	public static void createUserWishListDirectory(String userDirectory, String wishlist) {
+		
+		userWishListDirectory = new File(rootPathStr + File.separator + userDirectory + File.separator + wishlist);
+		
+		if(!userWishListDirectory.exists()) {
+			userWishListDirectory.mkdir();
+			System.out.println("Wish List was created at: " + userWishListDirectory.getAbsolutePath());
 		}
 		else {
-			System.out.println("Wish List already exists at: " + this.userWishListDirectory.getAbsolutePath());
+			System.out.println("Wish List already exists at: " + userWishListDirectory.getAbsolutePath() + " *** Directory not created!");
 		}
 	}
-		
+	
+	// Delete the user's directory
+	public static void deleteUserDirectory(String userDirectory) {
+		 deleteDirectory(new File(rootPathStr + File.separator + userDirectory));
+	}
+	
+	// Delete the content of the user's directory
+	private static boolean deleteDirectory(File directoryPath) {
+		if(directoryPath.exists()) {
+	      File[] userDirectoryContent = directoryPath.listFiles();
+	      for(int i=0; i<userDirectoryContent.length; i++) {
+	    	 // Found a directory - Call back the method recursively
+	         if(userDirectoryContent[i].isDirectory()) {
+	           deleteDirectory(userDirectoryContent[i]);
+	         }
+	         // Found a file - Delete it
+	         else {
+	           userDirectoryContent[i].delete();
+	         }
+	      }
+	    }
+	    return(directoryPath.delete());
+	}
+
+	
+	// Delete the user's wish list directory
+	public static void deleteUserWishListDirectory(String userDirectory, String wishlist) {
+		deleteFiles(new File(rootPathStr + File.separator + userDirectory + File.separator + wishlist));
+	}
+	
+	// Delete the files of the user's wish list directory
+	private static boolean deleteFiles(File directoryPath) {
+		if(directoryPath.exists()) {
+	      File[] userDirectoryContent = directoryPath.listFiles();
+	      for(int i=0; i<userDirectoryContent.length; i++) {
+	    	   // Delete a file
+	           userDirectoryContent[i].delete();
+	         }
+	      }
+	    return(directoryPath.delete());
+	}
+	
 	public static void main(String[] args) {
 		
-		FileManip fileManip = new FileManip();
-		
+		FileManip.createRootDirectory();
+	
 		// User1 home directory
-		fileManip.createUserDirectory("User1");
+		FileManip.createUserDirectory("User1");
 		// User1 creates a wish list - spring break
-		fileManip.createUserWishListDirectory("User1", "Spring Break - Cuba 2015");
+		FileManip.createUserWishListDirectory("User1", "Spring Break - Cuba 2015");
 		// User1 creates a wish list - easter
-		fileManip.createUserWishListDirectory("User1", "Easter");
+		FileManip.createUserWishListDirectory("User1", "Easter");
 		
 		// User2 home directory
-		fileManip.createUserDirectory("User2");
-		// User1 creates a wish list - spring break
-		fileManip.createUserWishListDirectory("User2", "Spring Break - The Real Cancun 2015");
-		// User1 creates a wish list - easter
-		fileManip.createUserWishListDirectory("User2", "Easter Bunnies");
+		FileManip.createUserDirectory("User2");
+		// User2 creates a wish list - spring break
+		FileManip.createUserWishListDirectory("User2", "Spring Break - The Real Cancun 2015");
+		// User2 creates a wish list - easter
+		FileManip.createUserWishListDirectory("User2", "Easter Bunnies");
 		
-		// User2 home directory
-		fileManip.createUserDirectory("User3");
-		// User1 creates a wish list - spring break
-		fileManip.createUserWishListDirectory("User3", "Spring Break - Burn Valadero 2015");
-		// User1 creates a wish list - easter
-		fileManip.createUserWishListDirectory("User3", "Easter Pink Bunnies");
+		// User3 home directory
+		FileManip.createUserDirectory("User3");
+		// User3 creates a wish list - spring break
+		FileManip.createUserWishListDirectory("User3", "Spring Break - Burn Valadero 2015");
+		// User3 creates a wish list - easter
+		FileManip.createUserWishListDirectory("User3", "Easter Pink Bunnies");
+
+		// Delete User1
+		FileManip.deleteUserDirectory("User1");
+		
+		// Delete User2 - Wish List: Easter Bunnies
+		FileManip.deleteUserWishListDirectory("User3", "Easter Pink Bunnies");
 
 	}
 
