8db808a7477f0f84e629d52493813f83a156697a
Schwarze Katze <derschwarzekatze@gmail.com>
2017-03-21 20:28:01 -0400

    Get the domain.

--- kernel/src/com/nslookup/NSLookUp.java
+++ kernel/src/com/nslookup/NSLookUp.java
@@ -8,14 +8,16 @@ public class NSLookUp {
 public void performNSLookup() {
         
         try {
-            
-            InetAddress inetHost = InetAddress.getByName("yahoo.ca");
+            String emailAddress = "sombody@facebook.com";
+            int emailAddressLength = emailAddress.length();
+            int atPosition = emailAddress.indexOf("@");
+            String domain = emailAddress.substring((atPosition+1), emailAddressLength);
+            InetAddress inetHost = InetAddress.getByName(domain);
             String hostName = inetHost.getHostName();
             System.out.println("The host name was: " + hostName);
-            System.out.println("The hosts IP address is: " + inetHost.getHostAddress());
-            
-        } catch(UnknownHostException ex) {
-            
+            System.out.println("The hosts IP address is: " + inetHost.getHostAddress());            
+        } 
+        catch(UnknownHostException ex) {           
             System.out.println("Unrecognized host");
         }
     }
